296c25b7d31c356685e216b23155d460be498695
nliyanage <nethmin.liyanage@gmail.com>
2024-04-02 18:03:27 -0700

    some very minimal amount of progress:/

--- load_tweets.py
+++ load_tweets.py
@@ -114,8 +114,12 @@ def insert_tweet(connection,tweet):
             user_id_urls = get_id_urls(tweet['user']['url'], connection)
 
         # create/update the user
-        sql = sqlalchemy.sql.text('''
-            ''')
+        sql = sqlalchemy.sql.text(INSERT INTO users (id_users, created_at, updated_at, id_urls, friends_count, listed_count, favourites_count, statuses_count, protected, verified, screen_name, name, location, description, withheld_in_countries) VALUES :id_users, :created_at, :updated_at, :id_urls, :friends_count, :listed_count, :favourites_count, :statuses_count, :protected, :verified, :screen_name, :name, :location, :description, :withheld_in_countries)
+
+
+        connection.execute(sql, {
+            'id_users': tweet['user']['id'],
+            'created_at': tweet['user']['created_at'],
 
         ########################################
         # insert into the tweets table
