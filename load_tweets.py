08b3d73bb9887889059f8d7cb02917e352b0c2d8
nliyanage <nethmin.liyanage@gmail.com>
2024-04-04 16:21:56 -0700

    test?

--- load_tweets.py
+++ load_tweets.py
@@ -112,7 +112,13 @@ def insert_tweet(connection,tweet):
             user_id_urls = get_id_urls(tweet['user']['url'], connection)
 
         # create/update the user
-        sql = sqlalchemy.sql.text("INSERT INTO users (id_users, created_at, updated_at, id_urls, friends_count, listed_count, favourites_count, statuses_count, protected, verified, screen_name, name, location, description, withheld_in_countries) VALUES (:id_users, :created_at, :updated_at, :id_urls, :friends_count, :listed_count, :favourites_count, :statuses_count, :protected, :verified, :screen_name, :name, :location, :description, :withheld_in_countries) ON CONFLICT DO NOTHING")
+        sql = sqlalchemy.sql.text('''
+        INSERT INTO users 
+        (id_users, created_at, updated_at, id_urls, friends_count, listed_count, favourites_count, statuses_count, protected, verified, screen_name, name, location, description, withheld_in_countries) 
+        VALUES 
+        (:id_users, :created_at, :updated_at, :id_urls, :friends_count, :listed_count, :favourites_count, :statuses_count, :protected, :verified, :screen_name, :name, :location, :description, :withheld_in_countries) 
+        ON CONFLICT (id_users)  DO NOTHING
+        ''')
 
         connection.execute(sql, {
             'id_users': tweet['user']['id'],
