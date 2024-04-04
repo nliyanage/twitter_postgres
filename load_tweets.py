54a87c4532ccadd5f2ae6313ead01ce943432104
nliyanage <nethmin.liyanage@gmail.com>
2024-04-04 09:58:36 -0700

    updates

--- load_tweets.py
+++ load_tweets.py
@@ -120,6 +120,19 @@ def insert_tweet(connection,tweet):
         connection.execute(sql, {
             'id_users': tweet['user']['id'],
             'created_at': tweet['user']['created_at'],
+            'updated_at': tweet['created_at'],
+            'id_urls':user_id_urls,
+            'friends_count':tweet['user']['friends_count'],
+            'listed_count':tweet['user']['listed_count'],
+            'favourites_count':tweet['user']['favourites_count'],
+            'statuses_count':tweet['user']['statuses_count'],
+            'protected':tweet['user']['protected'],
+            'verified':tweet['user']['verified'],
+            'screen_name':remove_nulls(tweet['user']['screen_name']),
+            'name':remove_nulls(tweet['user']['name']),
+            'location':remove_nulls(tweet['user']['location']),
+             'description':remove_nulls(tweet['user']['description']),
+            'withheld_in_countries':tweet['user'].get('withheld_in_countries', None),
 
         ########################################
         # insert into the tweets table
