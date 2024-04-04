bc588d78cff914a720ba01ca181f753d5809b8d9
nliyanage <nethmin.liyanage@gmail.com>
2024-04-04 12:22:29 -0700

    transaction fix

--- load_tweets.py
+++ load_tweets.py
@@ -104,7 +104,17 @@ def insert_tweet(connection,tweet):
     # insert tweet within a transaction;
     # this ensures that a tweet does not get "partially" loaded
     with connection.begin() as trans:
-
+        # skip tweet if it's already inserted
+        sql=sqlalchemy.sql.text('''
+        SELECT id_tweets 
+        FROM tweets
+        WHERE id_tweets = :id_tweets
+        ''')
+        res = connection.execute(sql,{
+            'id_tweets':tweet['id'],
+            })  
+        if res.first() is not None:
+            return
         ########################################
         # insert into the users table
         ########################################
